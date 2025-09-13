31d0e7e7f1ae850f5202edb4a9651e28deb632d8
Sanchitgg <sanchitgarg389@gmail.com>
2025-09-13 23:43:10 +0530

    preproces

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="featenf",
+    name="feateng",
     version="0.2.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
